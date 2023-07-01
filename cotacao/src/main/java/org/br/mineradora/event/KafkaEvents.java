a62476ded38501ac04ba63dc231ffe8ea92c4f25
pinhobrunodev <iaishowcs@gmail.com>
2023-07-01 10:11:37 -0300

    kafka

---
+++ cotacao/src/main/java/org/br/mineradora/event/KafkaEvents.java
@@ -0,0 +1,24 @@
+package org.br.mineradora.event;
+
+import org.br.mineradora.dto.QuotationDTO;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class KafkaEvents {
+
+    private final Logger LOG = LoggerFactory.getLogger(KafkaEvents.class);
+
+    @Channel("quotation-channel`") // canal que tem acesso a um topico do kafka
+    Emitter<QuotationDTO> quotationRequestEmitter; // emissor que vai enviar mensagens para o topico do kafka , informando tambem o tipo de msg
+
+    void sendNewKafkaEvent(QuotationDTO quotation) {
+        LOG.info("-- Enviando Cotação para Tópico Kafka --");
+        LOG.info(quotation.toString());
+        quotationRequestEmitter.send(quotation).toCompletableFuture().join();
+    }
+}
