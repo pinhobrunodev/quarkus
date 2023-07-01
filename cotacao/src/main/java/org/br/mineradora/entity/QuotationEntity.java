2bdf450430d5f524312633cda14407030f56e279
pinhobrunodev <iaishowcs@gmail.com>
2023-07-01 08:58:07 -0300

    quotation entity

---
+++ cotacao/src/main/java/org/br/mineradora/entity/QuotationEntity.java
@@ -0,0 +1,28 @@
+package org.br.mineradora.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "quotation")
+public class QuotationEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Date date;
+    @Column(name = "currency_price")
+    private BigDecimal currencyPrice;
+    @Column(name = "pct_change")
+    private String pctChange;
+    private String pair;
+}
